289f9ff3a712ed0c8051cf5fe1d11f407991a5ec
Juan Marcos Bertuggia <juan.bertuggia@gmail.com>
2023-06-02 23:45:05 -0300

    Proyecto Final JavaScript

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 //Creo la clase "Articulo", la voy a usar como plantilla para crear los objetos (artículos):
 
-class Articulo {
-    constructor(id, nombre, precio, imagen) {
+class Producto {
+    constructor(id, tipo, nombre, descripcion, precio, imagen) {
         this.id = id;
+        this.tipo = tipo;
         this.nombre = nombre;
+        this.descripcion = descripcion;
         this.precio = precio;
         this.imagen = imagen;
         this.cantidad = 1;
@@ -15,25 +17,225 @@ class Articulo {
 //Creo los ariculos para armar la pc:
 //Usé dolares como moneda para tener numeros mas chicos (sad)
 
-const cpuAmd = new Articulo(1, "Ryzen 5 5600", 326, "img/ryzen5.jpg");
-const cpuIntel = new Articulo(2, "Intel i5 3400", 518, "img/i5.jpg");
-const ram16 = new Articulo(3, "Corsair Vengeance 16gb (2x8) ddr4 3200", 69, "img/corsair16.jpg");
-const ram32 = new Articulo(4, "Kigston Fury 32gb (2x16) ddr4 3200", 188, "img/fury32.jpg");
-const videoNvidia = new Articulo(5, "Nvidia Geforce RTX 3060 ti", 793, "img/strix3060.jpg");
-const videoAmd = new Articulo(6, "Radeon 6700 XT", 739, "img/radeon6700.jpg");
-const teclado = new Articulo(7, "Corsair k70 Pro Mini", 169, "img/corsairKeyboard.jpg");
-const mouse = new Articulo(8, "Corsair m65 Pro Elite", 38, "img/corsairMouse.jpg");
-const headset = new Articulo(9, "Corsair hs80 Wireless", 133, "img/corsairHeadset.jpg");
+const cpuAmd = new Producto(1, "cpu", "Procesador AMD", "Ryzen 5 5600", 326, "img/ryzen5.jpg");
+const cpuIntel = new Producto(2, "cpu", "Procesador INTEL", "Intel i5 3400", 518, "img/i5.jpg");
+const ram16 = new Producto(3, "ram", "Memoria Corsair", "Vengeance 16gb (2x8) ddr4 3200", 69, "img/corsair16.jpg");
+const ram32 = new Producto(4, "ram", "Memoria Kingston", "Fury 32gb (2x16) ddr4 3200", 188, "img/fury32.jpg");
+const videoNvidia = new Producto(5, "gpu", "Placa de video Nvidia", "Asus Strix Geforce RTX 3060 ti", 793, "img/strix3060.jpg");
+const videoAmd = new Producto(6, "gpu", "Placa de video AMD", "Radeon 6700 XT", 739, "img/radeon6700.jpg");
+const teclado = new Producto(7, "perifericos", "Teclado Corsair", "K70 Pro Mini", 169, "img/corsairKeyboard.jpg");
+const mouse = new Producto(8, "perifericos", "Mouse Corsair", "M65 Pro Elite", 38, "img/corsairMouse.jpg");
+const headset = new Producto(9, "perifericos", "Heaset Corsair", "HS80 Wireless", 133, "img/corsairHeadset.jpg");
 
 
 //Creo un array que va a contener todos los objetos:
 
-const arrayArticulos = [cpuAmd, cpuIntel, ram16, ram32, videoAmd, videoNvidia, teclado, mouse, headset];
+const arrayProductos = [cpuAmd, cpuIntel, ram16, ram32, videoAmd, videoNvidia, teclado, mouse, headset];
 
 //Creo el array carrito (inicia vacío):
 
-let arrayCarrito = [];
+let arrayCarrito = JSON.parse(localStorage.getItem("arrayCarrito")) || [];
 
-//Muestro los productos en el HTML:
+/* //Cargamos el local Storage:
+if (localStorage.getItem(carritoStorage)) {
+    carritoStorage = JSON.parse (localStorage.getItem("carritoStorage"));
+} */
+
+//MOSTRAR LOS PRODUCTOS-------------------------------------------------------------------------------------------------------
+//Primero enlazo el array de prodcutos donde se van a mostrar en el HTML con js.
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+//Creo una función para recorrer el array de productos e ir mostrando cada uno en una card:
+const mostrarProductos = (productos) => {
+    productos.forEach(producto => {
+        let card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-sm-12", "p-2", "d-flex", "justify-content-center");
+        card.innerHTML = `<div class="card bg-dark text-white" style="width: 18rem;">
+                           <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+                           <div class="card-body">
+                               <h5 class="card-title">${producto.nombre}</h5>
+                               <p class="card-text">${producto.descripcion}.</p>
+                               <p class="card-text">USD$${producto.precio}.</p>
+                            </div>
+                               <a class="btn btn-danger" id="boton${producto.id}">Agregar al carrito</a>
+                            </div>
+                       </div>`
+
+        contenedorProductos.appendChild(card);
+
+        //Creo el evento al hacer click en el boton de "Agregar Carrito" ejecute la función de agregar productos al carrito:
+        const botonAgregarCarrito = document.getElementById(`boton${producto.id}`);
+        botonAgregarCarrito.addEventListener("click", () => {
+            agregarCarrito(producto.id);
+        })
+    });
+}
+
+mostrarProductos(arrayProductos);
 
 
+//Creo la función para agregar productos al carrito (agregarCarrito):
+const agregarCarrito = (id) => {
+    const productoEnCarrito = arrayCarrito.find(producto => producto.id === id);
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+
+    } else {
+        const producto = arrayProductos.find(producto => producto.id === id);
+        arrayCarrito.push(producto);
+    }
+    console.log(arrayCarrito);
+    saveLocal();
+     
+}
+
+
+
+//CARRITO------------------------------------------------------------------------------------------------------------------------------
+//Enlazo el boton del carrito con JS:
+const botonCarrito = document.getElementById("botonCarrito");
+botonCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+})
+
+//Enlazo los elementos del modal del HTML con JS:
+const modalCarrito = document.getElementById("modalCarrito");
+const modalProductosCarrito = document.getElementById("modalProductosCarrito");
+const modalTotalCarrito = document.getElementById("modalTotalCarrito");
+
+//Creo la función para mostrar los productos seleccionados en el carrito:
+const mostrarCarrito = () => {
+    modalProductosCarrito.innerHTML = "";
+    arrayCarrito.forEach(producto => {
+        let articulo = document.createElement("div");
+        let parrafo = document.createElement("p");
+        parrafo.innerText = `🔴 ${producto.nombre} (${producto.descripcion}) - Cantidad: ${producto.cantidad} = USD$${producto.precio}       `
+        //Este boton va a eliminar de a una unidad por producto (mirar la función mas abajo)
+        let boton = document.createElement("button");
+        boton.innerHTML = "🗑️";
+        boton.classList.add("boton-eliminar");
+        boton.addEventListener("click", () => {
+            eliminarUnaUnidadProducto(producto.id);
+        });
+        parrafo.appendChild(boton);
+        articulo.appendChild(parrafo);
+        modalProductosCarrito.appendChild(articulo);
+    });
+
+    //Creo una variable para usarla en el innerHTML del modal:
+    const totalCarrito = calcularTotal();
+
+    let total = document.createElement("p");
+    total.classList.add("total");
+    total.innerHTML = `Total: USD$${totalCarrito}`;
+
+    modalProductosCarrito.appendChild(total);
+}
+
+
+//Creo la función para eliminar una unidad de un producto del carrito:
+const eliminarUnaUnidadProducto = (id) => {
+    const producto = arrayCarrito.find(producto => producto.id === id);
+    if (producto.cantidad > 1) {
+        producto.cantidad--;
+    } else if (producto.cantidad === 1) {
+        let indice = arrayCarrito.indexOf(producto);
+        arrayCarrito.splice(indice, 1);
+    }
+    mostrarCarrito();
+    saveLocal();
+}
+
+ //Doy función a los dos botones dentro del modal (vaciar carrito y comprar):
+ const botonVaciar = document.getElementById("botonVaciar");
+ const botonComprar = document.getElementById("botonComprar");
+
+ botonVaciar.addEventListener("click", () => {
+    arrayCarrito = [];
+    mostrarCarrito();
+    saveLocal();
+ })
+
+ botonComprar.addEventListener("click", () => {
+        const total = calcularTotal();
+        if (arrayCarrito.length > 0) {
+            swal.fire({
+                title: "Gracias por comprar en Pc Lux!",
+                html: `<b>El total por su compra es de:</b> USD$${total}`,
+                icon: "success",
+            });
+            arrayCarrito = [];
+            mostrarCarrito();
+            saveLocal();
+        } else {
+            swal.fire({
+                title: "Carrito vacío!",
+                html: `<b>Por favor agregue productos para poder comprar</b>`,
+                icon: "error",
+            });
+        }
+    })
+
+//Creo una función que me de el total del carrito:
+const calcularTotal = () => {
+    let totalCompra = 0;
+    arrayCarrito.forEach(producto => {
+        totalCompra += producto.precio * producto.cantidad;
+    })
+    return totalCompra;
+}
+
+
+
+//NAVBAR-----------------------------------------------------------------------------------------------------------------------------
+//Se va a filtrar los productos a la hacer click sobre alguna de las opciones del navbar:
+const linkTodos = document.getElementById("linkTodos");
+const linkGpu = document.getElementById("linkGpu");
+const linkCpu = document.getElementById("linkCpu");
+const linkRam = document.getElementById("linkRam");
+const linkPerifericos = document.getElementById("linkPerifericos");
+
+linkTodos.addEventListener("click", () => {
+    contenedorProductos.innerHTML = "";
+    mostrarProductos(arrayProductos);
+}
+)
+
+linkGpu.addEventListener("click", () => {
+    const gpuFiltro = arrayProductos.filter((producto => producto.tipo === "gpu"));
+    contenedorProductos.innerHTML = "";
+    mostrarProductos(gpuFiltro);
+}
+)
+
+linkCpu.addEventListener("click", () => {
+    const cpuFiltro = arrayProductos.filter((producto => producto.tipo === "cpu"));
+    contenedorProductos.innerHTML = "";
+    mostrarProductos(cpuFiltro);
+}
+)
+
+linkRam.addEventListener("click", () => {
+    const ramFiltro = arrayProductos.filter((producto => producto.tipo === "ram"));
+    contenedorProductos.innerHTML = "";
+    mostrarProductos(ramFiltro);
+}
+)
+
+linkPerifericos.addEventListener("click", () => {
+    const perifericosFiltro = arrayProductos.filter((producto => producto.tipo === "perifericos"));
+    contenedorProductos.innerHTML = "";
+    mostrarProductos(perifericosFiltro);
+}
+)
+
+
+//LocalStorage, los creo y los guardo en una función para que sea mas facil:-----------------------------------------------------------
+
+//Guardo en localStorage:
+const saveLocal = () => {
+    localStorage.setItem("arrayCarrito", JSON.stringify(arrayCarrito));
+}
+
+//Restauro el localStorage:
+    // JSON.parse(localStorage.getItem("arrayCarrito"));
